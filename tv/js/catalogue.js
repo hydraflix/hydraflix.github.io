bad80de67e389c44afab05b941b5d1706bba0f31
eliooses <elio@netfilmx.com>
2015-10-10 20:33:55 -0300

    tv

--- tv/js/catalogue.js
+++ tv/js/catalogue.js
@@ -39,7 +39,7 @@ $(function () {
 		    	console.log("TVRage: "+movie.ids.tvrage);
 				// http://eztvapi.re/show/tt2575988
 				$.getJSON("http://crossorigin.me/http://eztvapi.re/show/"+movie.ids.imdb, function (data) {
-					console.log(data.episodes.torrents[0].url);
+					console.log(data.episodes);
 				});    	
 		    });
 		  }
